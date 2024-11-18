e53e58aaa40c86262eb37d4944e0cc857ad08737
dickson <dicksonanyaele1234@gmail.com>
2024-11-18 09:59:45 +0000

    chore: setup base

---
+++ src/main/java/com/finance/ftl/controller/FtlController.java
@@ -0,0 +1,16 @@
+package com.finance.ftl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class FtlController {
+
+  @GetMapping
+  public String dashboard() {
+    return "dashboard";
+  }
+
+}
